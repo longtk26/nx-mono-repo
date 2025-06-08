813e248faab569ac4a72bef292113fee2d3acc42
Pham Ngoc Long <ngoclong2663@gmail.com>
2025-06-08 11:30:38 +0700

    feat: pass eslint

--- apps/jobber-auth/src/app/modules/auth/auth.resolver.ts
+++ apps/jobber-auth/src/app/modules/auth/auth.resolver.ts
@@ -1,8 +1,4 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
-import { LoginModel } from './models/login.models';
+import { Resolver } from '@nestjs/graphql';
 
 @Resolver()
-export class AuthResolver {
-  @Mutation(() => LoginModel, { name: 'login' })
-  async login() {}
-}
+export class AuthResolver {}
